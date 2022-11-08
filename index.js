478675093a9b3928b1a1ee5382f6faa1bedefac5
LE1NART <55325418+LE1NART@users.noreply.github.com>
2022-11-08 15:59:46 +0100

    finished on side Keyboard functions
    -active cell is switching corretly if entered characer or if one is deleted
    -deleting letters is possible
    - acces to next line only if previous line is submited
    
    TODO: -When submitted -> Gamelogic
    -how to open file to read word out of wordlist for the game

--- index.js
+++ index.js
@@ -22,10 +22,72 @@ function sizeFonts(){
 function addLetter(char){
     //getting the cell that is currently active
     const current = document.getElementsByClassName("active")[0];
-
+    //adds the Char
     current.textContent = char;
+    
+    //if the current is not the last in the row
+    if(!current.classList.contains("last")){
+        //detoggle the current active
+        current.classList.toggle("active");
+
+        //toggle the sibling to get the next active
+        current.nextElementSibling.classList.toggle("active");
+    }
+}
+
+function deleteLetter(){
+    //getting the cell that is currently active
+    const current = document.getElementsByClassName("active")[0];
+
+    if(!current.classList.contains("first")){
+        //detoggle the current active
+        current.classList.toggle("active");
+
+        //toggle the sibling to get the next active
+        current.previousElementSibling.classList.toggle("active");
+    }
+
+    //if current is blank and not the first one
+    if((current.textContent == '\u00A0' || current.textContent == '&nbsp' )&& !current.classList.contains("first")){
+        //delete Char before
+        current.previousElementSibling.textContent = '\u00A0'
+    }
+    else{ //delete current Char
+        current.textContent = '\u00A0';
+    }
+}
+
+function submit(){
+
+    //TODO: Check for complete row & Gamelogic
+
+    //getting current active
+    let current = document.getElementsByClassName("active")[0];
+
+    //find the first Element from the next Row
+    var newRowStart  = newRow(current);
+
+
+
+    //toggle the current and the next active
     current.classList.toggle("active");
+    newRowStart.classList.toggle("active");
+}
 
-    current.nextElementSibling.classList.toggle("active");
-
+//recursive function to find the last Element of a row to return the next Sibling (first Element of the next Row)
+function newRow(current){
+    console.log(current);
+    //if current Element contains last
+    let el;
+    if(current.classList.contains("last")){
+        //returns next sibling
+        el = current.nextElementSibling
+    }
+    //if current not contains last
+    else{
+        //call same function with next Element as current
+        el = newRow(current.nextElementSibling);
+    }
+    //returns next sibling
+    return el;
 }
