b3c02ea1b045f36b4e4fb2fd83b35615549ff6de
LE1NART <55325418+LE1NART@users.noreply.github.com>
2022-11-14 15:28:31 +0100

    wordCheck Completed
    -check on submit if row is full and word exist
    -colors the single characters of the word, depending of the solution word

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-window.onload = (event) =>{sizeFonts();};
+window.onload = (event) =>{sizeFonts();}
 onresize = (event) => {sizeFonts();};
+
+
+let solution = getWordFromList();
+let sol = [] ;
+solution.then(result => result.split('').forEach(el => sol.push(el)));
+    
 
 //setting the Fonts of the Cells and the Buttons depending on the sizes
 function sizeFonts(){
@@ -57,26 +63,34 @@ function deleteLetter(){
     }
 }
 
-function submit(){
+async function submit(){
+    
+    
+    if(await completeRowCheck()){
+        checkWord();
 
-    //TODO: Check for complete row & Gamelogic
+        //getting current active
+        let current = document.getElementsByClassName("active")[0];
 
-    //getting current active
-    let current = document.getElementsByClassName("active")[0];
-
-    //find the first Element from the next Row
-    var newRowStart  = newRow(current);
+        //find the first Element from the next Row
+        var newRowStart  = newRow(current);
 
 
 
-    //toggle the current and the next active
-    current.classList.toggle("active");
-    newRowStart.classList.toggle("active");
+        //toggle the current and the next active
+        current.classList.toggle("active");
+        newRowStart.classList.toggle("active");
+    }
+    else{
+       console.log("not Fullfilled")
+    }
+    
+    
+    
 }
 
 //recursive function to find the last Element of a row to return the next Sibling (first Element of the next Row)
 function newRow(current){
-    console.log(current);
     //if current Element contains last
     let el;
     if(current.classList.contains("last")){
@@ -90,4 +104,105 @@ function newRow(current){
     }
     //returns next sibling
     return el;
+}
+
+function getWordFromList(){
+    //returns a random word (String) from the JSON File
+    return fetch('./words.json').then((response) => response.json()).then((json) => json.words[Math.floor(Math.random() * 6403)]);
+}
+
+async function checkWordInList(word){
+    //checks if the given word is included in the list
+    return fetch('./words.json').then((response) => response.json()).then((json) => json.words.includes(word));
+}
+
+async function completeRowCheck(){
+    //checks if all the cells of the current row are filled and if the word exist
+    //returns true or false
+    //creates a word out of the letters of every cell
+    let word = [];
+    //checks for non empty cells
+    let first = findFirst(document.getElementsByClassName("active")[0]);
+    for(let a = 0;a<5;a++){
+        //check if empty
+        if(first.textContent == '\u00A0'){
+            return false;
+        }
+        //adds character to the word array
+        word.push(first.textContent)
+        //takes next cell
+        first = first.nextElementSibling;
+    }
+    
+    //waits if word is in the list
+    if(await checkWordInList(word.toString().replaceAll(",",""))){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function getRowWordAndElements(){
+    //returns the characters of the input and the cells as an array
+    let first = findFirst(document.getElementsByClassName("active")[0]);
+    let divs = [];
+    let chars = [];
+    for(let a=0;a<5;a++){
+        divs.push(first);
+        chars.push(first.textContent);
+        first = first.nextElementSibling;
+    }
+    return [chars,divs];
+}
+
+function findFirst(current){
+    //if current Element contains first
+    let el;
+    if(current.classList.contains("first")){
+        //returns next sibling
+        return current;
+    }
+    //if current not contains last
+    else{
+        //call same function with next Element as current
+        el = findFirst(current.previousElementSibling);
+    }
+    //returns next sibling
+    return el;
+}
+
+function checkWord(){
+    //fills the cells with the color for the letters
+
+    let wordEl = getRowWordAndElements();
+    let word = wordEl[0];
+    let el = wordEl[1];
+    let workSol = sol.map(x => x);
+    
+
+    //check for correct
+    for(let a = 0;a<5;a++){
+        if(word[a] == workSol[a]){
+            word[a] = '\.';
+            workSol[a] = '\,'; 
+            el[a].classList.toggle("correct");
+        }
+    }
+    //check for partial
+    for(let a = 0;a<5;a++){
+        for(let b=0;b<5;b++){
+            if(word[a] == workSol[b]){
+            word[a] = '\.';
+            workSol[b] = '\,'; 
+            el[a].classList.toggle("partial");
+            }
+        }
+    }
+    //all leftover characters get the wrong
+    for(let a = 0;a<5;a++){
+        if(!(el[a].classList.contains("partial")|el[a].classList.contains("correct"))){
+            el[a].classList.toggle("wrong");
+        }
+    }
 }
